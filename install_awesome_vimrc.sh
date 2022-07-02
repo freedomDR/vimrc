4974bfaf98608757b7bb66422fd251b2c8a2766b
freedomDR <1640145602@qq.com>
2022-07-02 12:43:20 +0000

    update my config

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -15,6 +15,6 @@ source ~/.vim_runtime/vimrcs/extended.vim
 try
   source ~/.vim_runtime/my_configs.vim
 catch
-endtry' > ~/.vimrc
+endtry' > ~/.config/nvim/init.vim
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
